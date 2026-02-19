b3ae02082a33c74bbefa82e7d6b3e343f12f0232
Lester Lou Reyes <llreyes.589@gmail.com>
2026-02-19 15:12:01 +0800

    2.6: The Phonebook Step 1

--- part2/courseinfo/src/Content.jsx
+++ part2/courseinfo/src/Content.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import Part from "./Part";
 
 const Content = ({ parts }) => {
-  console.log({ parts });
   return (
     <div>
       {parts.map((part) => (
-        <Part part={part} />
+        <Part key={part.id} part={part} />
       ))}
     </div>
   );
