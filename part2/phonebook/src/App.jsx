6f8e481bf618ee557127f20356bfa1ad70222dc8
Lester Lou Reyes <llreyes.589@gmail.com>
2026-02-19 18:16:34 +0800

    2.11: The Phonebook Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,17 +2,22 @@ import { useState } from "react";
 import Filter from "./Filter";
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
+import axios from "axios";
+import { useEffect } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchString, setSearchString] = useState("");
+
+  useEffect(() => {
+    const eventHandler = (response) => {
+      setPersons(response.data);
+    };
+    const promise = axios.get("http://localhost:3001/persons");
+    promise.then(eventHandler);
+  }, []);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
--- part2/phonebook/src/Filter.jsx
+++ part2/phonebook/src/Filter.jsx
@@ -1,4 +1,12 @@
 const Filter = ({ searchString, handleSearchPerson }) => {
+  //   setTimeout(() => {
+  //     console.log("loop..");
+  //     let i = 0;
+  //     while (i < 99999999999) {
+  //       i++;
+  //     }
+  //     console.log("end");
+  //   }, 5000);
   return (
     <div>
       filter shown with
