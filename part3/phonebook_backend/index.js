1f797b33e15de732af735c607d6f48738a950989
Lester Lou Reyes <llreyes.589@gmail.com>
2026-02-28 14:18:18 +0800

    3.18*: Phonebook database step 6

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -58,9 +58,11 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`,
-  );
+  Person.find({}).then((persons) => {
+    response.send(
+      `<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`,
+    );
+  });
 });
 
 app.get("/api/persons/:id", (request, response, next) => {
